eb4432a134dc3cdaaec5264086c2da986620676d
YunpuZhang <48321652+YunpuZhang@users.noreply.github.com>
2023-04-05 21:44:23 -0400

    TRUS Control ALl Finished

--- ROS_practice.m
+++ ROS_practice.m
@@ -2,4 +2,17 @@ clc;
 clear all;
 close all;
 
-% rosinit;
+% rosshutdown
+% rosinit('10.203.6.175');
+% pause(2);
+% chatterpub = rospublisher("/num","std_msgs/Float64","DataFormat","struct")
+% pause(1)
+% chattermsg = rosmessage(chatterpub);
+% chattermsg.Data = 0.1;
+% send(chatterpub,chattermsg)
+
+rosshutdown;
+rosinit
+pause(2);
+
+
